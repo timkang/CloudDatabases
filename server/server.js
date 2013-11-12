1127cc6d8aee770730d96934fe6bb5aca5d7007c
ed <ed@ed-VirtualBox.(none)>
2013-11-11 18:22:59 -0800

    Added changes to insert/check sql queries for mysql

--- server/server.js
+++ server/server.js
@@ -1,30 +1,54 @@
 var http = require("http"),
 url = require("url"),
 fs = require("fs"),
-mysql = require("mysql");
+mysql = require("mysql")
+queryString = require("querystring"),
+uri = require("uri");
 
-var connection = mysql.createConnection({ 
+var connectionToMySQL = mysql.createConnection({ 
    user: "root", 
    password: "", 
    database: "mySQLTest"
 }); 
 
-http.createServer(function(request, response) {
+/*
+    Sample link: http://localhost:8888/mysql?query=select+*+from+foo
+ */
+
+http.createServer(function(request, response, nyar) {
     response.writeHead(200, {"Content-Type": "text/plain"});
 
-    var parts = url.parse(request.url, true).pathname;
+    // Figure out which databse to call
+    var databasePathName = url.parse(request.url, true).pathname;
+    var queryPathName = url.parse(request.url, true).query;
+    var queryPathNameStringify = queryString.stringify(queryPathName);
+
+    // Parse out the query 
+    var fullQueryString = queryPathNameStringify.replace("query=", "");
+    var finalQueryToDatabase = decodeURIComponent(fullQueryString);
+    console.log("Query to be executed: \'"+finalQueryToDatabase+"\'");
+    
+    // Find out which database needs to be executed
+    if (databasePathName == "/mysql") {
+        console.log("mysql database needs to be executed")
+
+        connectionToMySQL.query(finalQueryToDatabase+";", function (error, rows, fields) {
+            // response.writeHead(200, {'Content-Type': 'x-application/json'}); 
+            console.log(JSON.stringify(rows));
+        });
+    }
+    else if (databasePathName == "/redis") {
+        console.log("redis database needs to be executed")
+
+    }
+    else if (databasePathName == "/mongodb") {
+        console.log("mongodb database needs to be executed")
+    }
+    else {
+        console.log("Nothing was found");
+    }
 
 
-    request.on('end', function () {
-        response.writeHead(200, {'Content-Type': 'x-application/json'}); 
-
-        connection.query('SELECT * FROM foo;', function (error, rows, fields) { 
-            response.write("Hello World\n");
-
-            response.write(JSON.stringify(rows));
-            response.end();      
-    	});
-    });
 
     // response.write('Here is your data: ' + parts + '\n');
 
@@ -33,6 +57,6 @@ http.createServer(function(request, response) {
         data = 1;
         fs.writeFile('test.txt', data);
     });
-   // response.end();
+   response.end();
 }).listen(8888);
 
