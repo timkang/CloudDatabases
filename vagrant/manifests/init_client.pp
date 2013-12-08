e3e547906ba8966a57567c4f66b631cdaf46ee1b
Ed <ey7180@gmail.com>
2013-12-07 16:02:46 -0800

    Removed unused databases in YCSB folder Modified puppet files to include predefined values ("/etc/hosts, /etc/mongodb.conf, /etc/version" Included start up script to start mongo/mongos

--- vagrant/manifests/init_client.pp
+++ vagrant/manifests/init_client.pp
@@ -1,9 +1,3 @@
-exec { "dist-upgrade":
-   environment => ["DEBIAN_FRONTEND=noninteractive"],
-   command => "/usr/bin/apt-get -y -o Dpkg::Options::=\"--force-confdef\" -o Dpkg::Options::=\"--force-confold\" dist-upgrade",
-   timeout => 0
-}
-
 exec { "apt-update":    
   command => "/usr/bin/apt-get update",
   timeout => 0
@@ -16,16 +10,11 @@ exec { "copy_ycsb":
 
 exec { "install_ycsb":
   cwd => "/home/vagrant/YCSB",
-  command => "/usr/bin/mvn package clean",
+  command => "/usr/bin/mvn package",
   timeout => 0
 }
 
-Exec["apt-update"] -> Exec["dist-upgrade"] -> Package <| |> -> Exec["copy_ycsb"] -> Exec["install_ycsb"]
-
-package {
-   "git":
-      ensure => installed
-}
+Exec["apt-update"] -> Package <| |> -> Exec["copy_ycsb"] -> Exec["install_ycsb"]
 
 package {
   "maven":
@@ -35,4 +24,50 @@ package {
 package {
   "openjdk-7-jdk":
     ensure => installed
+}
+
+package {
+   "mongodb":
+      ensure => installed
+}
+
+file {
+   "/home/vagrant/start_mongos.sh":
+      content => "sudo mongos --configdb cfg_server:27017 --port 27018",
+      mode => 0777
+}
+
+file {
+   "/etc/hosts":
+   content =>
+"127.0.0.1 localhost
+127.0.0.1 precise64
+192.168.33.10 cfg_server
+192.168.33.11 shard_01
+192.168.33.12 shard_02"
+}
+
+file {
+   "/etc/version":
+      content => "mongodb_config_server\n"
+}
+
+file {
+  "/etc/mongodb.conf":
+   content => 
+"# mongodb.conf
+
+# Where to store the data.
+dbpath=/var/lib/mongodb
+
+#where to log
+#logpath=/var/log/mongodb/mongodb.log
+#logappend=true
+
+port = 27017
+configsvr = true
+chunkSize = 2
+
+# Enable journaling, http://www.mongodb.org/display/DOCS/Journaling
+journal=true"
 }
---
+++ vagrant/manifests/init_mongodb_shard_01.pp
@@ -0,0 +1,52 @@
+exec { "apt-update":    
+   command => "/usr/bin/apt-get update",
+   timeout => 0
+}
+
+Exec["apt-update"] -> Package <| |>
+
+package {
+   "mongodb":
+      ensure => installed
+}
+
+file {
+   "/home/vagrant/start_mongo.sh":
+      content => "sudo mongo --host cfg_server --port 27018",
+      mode => 0777
+}
+
+file {
+   "/etc/hosts":
+   content =>
+"127.0.0.1 localhost
+127.0.0.1 precise64
+192.168.33.10 cfg_server
+192.168.33.11 shard_01
+192.168.33.12 shard_02"
+}
+
+file {
+   "/etc/version":
+      content => "mongodb_shard_01\n"
+}
+
+file {
+  "/etc/mongodb.conf":
+    content => 
+"# mongodb.conf
+
+# Where to store the data.
+dbpath=/var/lib/mongodb
+
+#where to log
+#logpath=/var/log/mongodb/mongodb.log
+#logappend=true
+
+port = 27017
+shardsvr = true
+chunkSize = 2
+
+# Enable journaling, http://www.mongodb.org/display/DOCS/Journaling
+journal=true"
+}
